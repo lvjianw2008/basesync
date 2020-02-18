3c143c6d7222146f7acb00e92a6639463572e254
lvjianw <lvjianw@qq.com>
2020-02-18 14:43:19 +0800

    合并漏掉的代码 补充提交 LV

--- src/main/java/com/bd/basesync/service/IJobLogService.java
+++ src/main/java/com/bd/basesync/service/IJobLogService.java
@@ -5,6 +5,6 @@ import com.bd.basesync.entity.JobLog;
 import com.github.pagehelper.PageInfo;
 
 public interface IJobLogService {
-    public PageInfo<JobLog> queryJobLogByJobName(int pageNum, int pageSize,String jobName);
+    public PageInfo<JobLog> queryJobLogByJobName(int pageNum, int pageSize, String jobName);
     public void insertJobLog(JobLog jobLog);
 }
