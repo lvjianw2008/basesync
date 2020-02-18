0b779027305f4fd358fbe352e974725e39b07ef6
Lvj-Laptop <lvjianw@qq.com>
2020-02-18 09:39:31 +0800

    测试类提交 LV

--- src/test/java/com/bd/basesync/BasesyncApplicationTests.java
+++ src/test/java/com/bd/basesync/BasesyncApplicationTests.java
@@ -3,6 +3,9 @@ package com.bd.basesync;
 import com.bd.basesync.entity.JobLog;
 import com.bd.basesync.service.IJobLogService;
 import com.bd.basesync.util.IDGenerator;
+import com.bd.basesync.util.StringUtil;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +45,31 @@ public class BasesyncApplicationTests {
 		jobLog.setJOB_STATUS("SUCCESS");
 		jobLog.setJOB_LOG_DETAIL("任务细节");
 		iJobLogService.insertJobLog(jobLog);
+	}
+
+	@Test
+	public void test1(){
+		String fdAllotRight = "{\"taskId\":\"169b95f54dad559f938ca9842518d46a\",\"processId\":\"169b95f543ff5dcb290623e48d4a721a\",\"activityType\":\"draftWorkitem\",\"operationType\":\"drafter_submit\",\"param\":{\"operationName\":\"提交文档\",\"notifyLevel\":\"3\",\"auditNote\":\"1\\n1\",\"auditNoteFdId\":\"169b95ffb3f7bb89e0b4ddd421f8289a\",\"identityId\":\"15aa1288960a18b91449f874792a05b1\",\"notifyOnFinish\":true,\"dayOfNotifyDrafter\":\"0\",\"hourOfNotifyDrafter\":\"0\",\"minuteOfNotifyDrafter\":\"0\"}}";
+		JSONObject obj =  JSONObject.fromObject(fdAllotRight);
+		JSONObject paramobj = (JSONObject) obj.get("param");
+		if(paramobj!=null){
+			String auditNote = (String) paramobj.get("auditNote");
+			if(StringUtil.isNotNull(auditNote)){
+				paramobj.put("auditNote","test");
+			}
+		}
+		System.out.println(obj.toString());
+	}
+
+	@Test
+	public void test2() {
+		JSONObject json = new JSONObject();
+		JSONArray leaderArray = new JSONArray();//直属领导审批
+		JSONObject leaderObject = new JSONObject();
+		leaderObject.put("Id","123" );
+		leaderArray.add(leaderObject);
+		json.put("fd_this_leader",  leaderArray);
+		System.out.println(json.toString());
 	}
 }
 
